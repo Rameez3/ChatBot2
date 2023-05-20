106fb49eddb9f1391191d0c432ebccdc27c56eff
Rameez3 <47284902+Rameez3@users.noreply.github.com>
2023-05-20 16:16:29 -0500

    Added a generateRandomUsername() function that assigns a random username to a user that visits the site. This is allows the user to stay anonymous

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
+
+
+
+function generateRandomUsername() {
+  let adjectives = ["Diligent", "Cheerful", "Sunny", "Supreme", "Magnificent"];
+  let nouns = ["Daisy", "Sunflower", "Platypus", "Butterfly", "Comet"];
+  
+  let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  
+  let username = randomAdjective + " " + randomNoun;
+  return username;
+}
+
 function App() {
-  return(
-    <div>
-      <h1>HII</h1>
-    </div>
-  )
+  return (
+    <h1>{generateRandomUsername()}</h1>
+  );
 }
 
 export default App;
