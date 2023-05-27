398c6018578e241eab2cd73ba0bbe39ecd4c0f9a
Rameez3 <47284902+Rameez3@users.noreply.github.com>
2023-05-26 21:58:15 -0500

    Added a background picture and overlaid the buttons over it to make the website nicer

--- src/components/IndexButtons.js
+++ src/components/IndexButtons.js
@@ -1,15 +1,33 @@
 import Stack from 'react-bootstrap/Stack';
 import PurpleButton from "./PurpleButton";
+import backgroundImage from '../assets/background.jpg';
 
 function IndexButtons() {
+  const bodyDivStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const buttonContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    gap: "10px",
+  };
+
   return (
-    <>
-    <Stack direction="horizontal" gap={3} className="justify-content-center d-flex mt-5">
+    <div style={bodyDivStyle}>
+      <Stack direction="horizontal" gap={3} style={buttonContainerStyle}>
         <PurpleButton link="#" title="Talk with RiverBot" />
         <PurpleButton link="#" title="Talk with a Friend" />
         <PurpleButton link="#" title="Find Local, Professional Help" />
-    </Stack>
-    </>
+      </Stack>
+    </div>
   );
 };
 
