f7883d23b922ab6dd1646279333aee328c047a18
Rameez3 <47284902+Rameez3@users.noreply.github.com>
2023-05-30 19:07:58 -0500

    Added a footer so the user can access other pages of the website easily

--- src/pages/ChatBot.js
+++ src/pages/ChatBot.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import NavBar from "../components/HomeComponents/NavBar";
+import PurpleButton from "../components/HomeComponents/PurpleButton";
 import ChatWindow from "../components/ChatComponents/ChatWindow";
 function ChatBot() {
-    return(
+    return (
         <Container fluid className="p-0">
             <NavBar />
             <ChatWindow />
+
+            <footer className="mt-5 d-flex justify-content-between">
+                <PurpleButton link="/" title="< Home"></PurpleButton>
+                <div className="ml-auto">
+                    <PurpleButton link="#" title="Professional Help >"></PurpleButton>
+                    <PurpleButton link="#" title="Talk with a Friend >"></PurpleButton>
+                </div>
+            </footer>
+
         </Container>
     )
 };
