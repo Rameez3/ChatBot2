478ce0664d20e9518d9fb035f5c64a7296395e65
Rameez3 <47284902+Rameez3@users.noreply.github.com>
2023-06-08 15:09:30 -0500

    Modified both of these files to accomodate each other by communicating with their openai api

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,41 @@
+const { Configuration, OpenAIApi } = require('openai');
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 const port = process.env.PORT;
 const uri = process.env.URI;
 const dbName = process.env.dbName;
 const collectionName = process.env.collectionName;
 
 const client = new MongoClient(uri);
+
+// Initialize the bot and assign its persona
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+openai = new OpenAIApi(configuration); // Store the instance in the openai variable
+
+async function initializeBot() {
+  const persona = "You are a Rogerian psychotherapist. You listen empathetically and provide support. You avoid giving direct advice and encourage the user to explore their feelings. Keep responses brief and speak in the first person like a conversation. Introduce yourself and help me with my mental health. Your name is Riverbot. Do not refer to yourself as a psychotherapist, but instead use pronouns like I.";
+  const initial_bot_response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: persona,
+    temperature: 0,
+    max_tokens: 150,
+  });
+  console.log("Initial Response", initial_bot_response.data.choices[0].text);
+};
+
+//Run the function that will intialize the bot and assign its persona
+initializeBot();
 
 app.get('/api/data', async (req, res) => {
   try {
@@ -61,6 +85,27 @@ app.post('/api/login', async (req, res) => {
     }
   } catch (error) {
     console.error('Failed to verify login:', error);
+    res.sendStatus(500);
+  }
+});
+
+app.post('/api/chat', async (req, res) => {
+  try {
+    const message = req.body.message; // Get the user's message from the request body
+
+    const completion = await openai.createCompletion({ // Use the stored openai instance
+      model: "text-davinci-003",
+      prompt: message,
+      max_tokens: 150,
+    });
+
+    const botResponse = completion.data.choices[0].text; // Get the bot's response
+    console.log("Bot's Response to the User's Message:", botResponse);
+
+    res.json({ response: botResponse }); // Send the bot's response as JSON
+
+  } catch (error) {
+    console.error('Failed', error);
     res.sendStatus(500);
   }
 });
--- src/components/ChatComponents/ChatWindow.js
+++ src/components/ChatComponents/ChatWindow.js
@@ -1,19 +1,21 @@
 import React, { useRef, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Container from "react-bootstrap/Container";
-
+import Spinner from 'react-bootstrap/Spinner';
 import ImageButton from "../../assets/send.png";
-import "./chatwindow.css"
+import axios from "axios";
+import "../../componentsStyling/ChatComponentsStyling/chatwindow.css";
 
 function ChatWindow() {
   const textareaRef = useRef(null);
   const [message, setMessage] = useState(""); // The message that the user will send to the bot.
-  
+  const [isLoading, setIsLoading] = useState(false); // Flag to track API request loading state
+
   // Function that will disable the button if the message is empty and enable it if the message is not empty.
   const handleButtonStatus = (buttonStatus) => {
-    let button = document.getElementById("send-button");
-    buttonStatus ? button.disabled = true : button.disabled = false;
-  }
+    let button = document.  getElementById("send-button");
+    buttonStatus ? (button.disabled = true) : (button.disabled = false);
+  };
 
   // The function that will change the height of the textarea as the user enters more of their message.
   // Also keeps track of the message and disables the send button if the message is empty.
@@ -27,7 +29,7 @@ function ChatWindow() {
     textarea.style.height = `${textarea.scrollHeight}px`; // Set the height to match the content
 
     if (textarea.value === "") {
-      //If the message is empty, disable the send button.
+      // If the message is empty, disable the send button.
       handleButtonStatus(true);
     } else {
       // If the message is not empty, enable the send button.
@@ -38,31 +40,44 @@ function ChatWindow() {
   const handleSendButton = () => {
     // Clear the textarea and reset the height.
     textareaRef.current.value = "";
-
-    // Append the blue chat bubbble onto the conversation div.
+  
+    // Append the blue chat bubble onto the conversation div.
     let conversation = document.getElementById("conversation");
     let userMessage = document.createElement("p");
-
+    let botMessage = document.createElement("p");
+  
     userMessage.className = "user-message";
     userMessage.textContent = message;
     conversation.appendChild(userMessage);
+  
+    // Show the loading spinner
+    setIsLoading(true);
 
-    // Simulate the bot's response to the user's message
-    let botMessage = document.createElement("p");
-
-    botMessage.className = "bot-message";
-    botMessage.textContent = "This is the bot's response!";
-    conversation.appendChild(botMessage);
-
-
-  }
+    // Make API request to /api/chat
+    axios.post('http://localhost:3000/api/chat', message)
+    .then(response => {
+      if (response.status === 200) {
+        // Get the bot's response from the response data
+        const botResponse = response.response;
+        // Print the bot's response onto the chat bubble 
+        botMessage.className = "bot-message";
+        botMessage.textContent = botResponse;
+        conversation.appendChild(botMessage);
+      } else {
+        alert('Failed to retrieve bot response');
+      }
+    })
+    .catch(error => {
+      console.error('Error retrieving bot response:', error);
+      alert('Failed to retrieve bot response');
+    });
+  };
+  
 
   return (
     <div className="chat-window">
       <h1>Riverbot</h1>
-      <div className="conversation" id="conversation">
-        <p className="user-message">This is the user's message!</p>
-      </div>
+      <div className="conversation" id="conversation"></div>
 
       <Container className="send-area">
         <textarea
@@ -72,8 +87,23 @@ function ChatWindow() {
           cols="83"
           onChange={handleTextareaChange}
         ></textarea>
-        <Button variant="primary" size="lg" id="send-button">
-          <img src={ImageButton} alt="Send" height="25px" onClick={handleSendButton}/>
+        <Button
+          variant="primary"
+          size="lg"
+          id="send-button"
+          onClick={handleSendButton}
+          disabled={isLoading} // Disable the button when the API request is loading
+        >
+          <img src={ImageButton} alt="Send" height="25px" id="send-img" />
+
+          {isLoading && (
+            <Spinner
+              animation="border"
+              variant="light"
+              size="sm"
+              id="send-spinner"
+            />
+          )}
         </Button>
       </Container>
     </div>
